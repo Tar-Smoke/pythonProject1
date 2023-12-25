13547111c590c4f00dc123595d0d0209e524e851
Tar-Smoke <dymkotaras876@gmail.com>
2023-12-25 13:57:34 +0200

    last version

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 # This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}!')  # Press Ctrl+F8 to toggle the breakpoint.
-
+from pythagoras_trousers import pt
+a = [5, 3, 4]
+b = [6, 8, 10]
+c = [100, 3, 65]
 # Press the green button in the gutter to run the script.
+def test_first(x):
+    print(pt(x))
 if __name__ == '__main__':
-    print_hi('Terry')
-
+    test_first(a)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ pythagoras_trousers.py
@@ -0,0 +1,14 @@
+# The function takes an array and returns true or false.
+
+def pt(any_array):
+    x = False
+    if len(any_array) == 3:
+        any_array.sort()
+        if (any_array[1] + any_array[0]) > any_array[2]:
+            x = True
+        else:
+            x = False
+    else:
+        print("Try again")
+
+    return x
